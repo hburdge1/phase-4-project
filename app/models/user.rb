418cc0fab7dfa5cbfab5f127b0df967ec13e2ac5
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-03-04 09:51:13 -0800

    initial auth outline added

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, presence: true, uniqueness: true
+    validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness:true
+end
